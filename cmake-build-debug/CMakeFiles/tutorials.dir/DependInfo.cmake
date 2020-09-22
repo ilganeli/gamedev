ca8a5ba9ed76b08dcf933500a5c38786ae24efc8
Ilya Ganelin <ilganeli@gmail.com>
2020-09-22 19:40:24 -0400

    adding files

---
+++ cmake-build-debug/CMakeFiles/tutorials.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/ilganeli/code/gamedev/tutorials/15_rotation_and_flipping/15_rotation_and_flipping.cpp" "/Users/ilganeli/code/gamedev/tutorials/cmake-build-debug/CMakeFiles/tutorials.dir/15_rotation_and_flipping/15_rotation_and_flipping.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/sdl2/2.0.12_1/include/SDL2"
+  "/usr/local/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tutorials.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tutorials.dir/15_rotation_and_flipping/15_rotation_and_flipping.cpp.o"
+  "tutorials"
+  "tutorials.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tutorials.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
